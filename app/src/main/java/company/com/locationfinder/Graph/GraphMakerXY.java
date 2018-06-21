0a855abbf555f7c41c2477f39d9ef51add686950
Dushan <dushan.14@cse.mrt.ac.lk>
2018-06-22 00:18:00 +0530

    chart properties changed

--- app/src/main/java/company/com/locationfinder/Graph/GraphMakerXY.java
+++ app/src/main/java/company/com/locationfinder/Graph/GraphMakerXY.java
@@ -5,6 +5,9 @@ import android.view.View;
 
 import com.github.mikephil.charting.charts.ScatterChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.ScatterData;
 import com.github.mikephil.charting.data.ScatterDataSet;
@@ -28,16 +31,12 @@ public class GraphMakerXY {
     public static void drawGraph(View viewById, Coordinate2D beacon_1, Coordinate2D beacon_2, Coordinate2D beacon_3){
 
         chart = (ScatterChart) viewById;
+        setGraphProperties();
 
-        Description descriptionX = new Description();
-        descriptionX.setText("X");
-        descriptionX.setPosition(10,0);
 
-        Description descriptionY = new Description();
-        descriptionY.setText("X");
 
-        chart.setDescription(descriptionX);
-        chart.setDescription(descriptionY);
+//        chart.setDescription(descriptionX);
+//        chart.setDescription(descriptionY);
 
         final List<Entry> beacon_1Entry = new ArrayList<Entry>();
         beacon_1Entry.add(new Entry(beacon_1.getX(), beacon_1.getY()));
@@ -47,6 +46,8 @@ public class GraphMakerXY {
         beacon_1_.setValueTextColor(Color.BLACK);
         beacon_1_.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
         beacon_1_.setScatterShapeSize(50);
+        beacon_1_.setScatterShapeHoleRadius(5);
+        beacon_1_.setDrawValues(false);
 
         final List<Entry> beacon_2Entry = new ArrayList<Entry>();
         beacon_2Entry.add(new Entry(beacon_2.getX(), beacon_2.getY()));
@@ -56,6 +57,8 @@ public class GraphMakerXY {
         beacon_2_.setValueTextColor(Color.BLACK);
         beacon_2_.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
         beacon_2_.setScatterShapeSize(50);
+        beacon_2_.setScatterShapeHoleRadius(5);
+        beacon_2_.setDrawValues(false);
 
         final List<Entry> beacon_3Entry = new ArrayList<Entry>();
         beacon_3Entry.add(new Entry(beacon_3.getX(), beacon_3.getY()));
@@ -65,9 +68,37 @@ public class GraphMakerXY {
         beacon_3_.setValueTextColor(Color.BLACK);
         beacon_3_.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
         beacon_3_.setScatterShapeSize(50);
+        beacon_3_.setScatterShapeHoleRadius(5);
+        beacon_3_.setDrawValues(false);
+        beacon_3_.setDrawIcons(true);
+
 
         ScatterDataSet[] dataSets={beacon_1_,beacon_2_,beacon_3_};
         addToGraph(dataSets);
+    }
+
+    private static void setGraphProperties(){
+
+        Description description = new Description();
+        description.setText("");
+
+        chart.setDescription(description);
+
+        chart.getAxisRight().setEnabled(false);
+        chart.setDrawGridBackground(false);
+
+        chart.getAxisLeft().setDrawGridLines(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        chart.setDrawBorders(true);
+
+        chart.getLegend().setForm(Legend.LegendForm.CIRCLE);
+        chart.getLegend().setXEntrySpace(30);
+
+//        chart.setAutoScaleMinMaxEnabled(false);
+
     }
 
     public static void addPoint(Coordinate2D point) {
@@ -76,6 +107,9 @@ public class GraphMakerXY {
         point_ = new ScatterDataSet(point_Entry, "Point");
         point_.setColor(Color.RED);
         point_.setValueTextColor(Color.BLACK);
+        point_.setScatterShape(ScatterChart.ScatterShape.CIRCLE);
+        point_.setScatterShapeSize(70);
+        point_.setDrawValues(false);
 
         ScatterDataSet[] dataSets={beacon_1_,beacon_2_,beacon_3_,point_};
         addToGraph(dataSets);
